6f541381d31492ec2859a7af9576453516cdcf56
Damini varu <daminivaru@Daminis-MBP.attlocal.net>
2019-10-13 19:30:36 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create([{ first_name: "Lindey", last_name: "Stirling" }, { first_name: "Daenerys", last_name: "Targaryen" }]
